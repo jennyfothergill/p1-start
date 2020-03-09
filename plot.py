0bac6035e1827077329e30bf8a9b26f3e812351b
Jenny Fothergill <jennyfothergill@gmail.com>
2020-03-09 14:38:09 -0600

    add error handling for file load

--- plot.py
+++ plot.py
@@ -2,51 +2,61 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-try:
-    filename = sys.argv[1]        # Stores ARG1 in filename, as in: $ python plot.py ARG1 ARG2 
-except IndexError:
-    print(
-"No filename provided! Please enter a file name, e.g.\n",
-"$ python plot.py FILENAME"
-)
+def main():
+    try:
+        filename = sys.argv[1]        # Stores ARG1 in filename, as in: $ python plot.py ARG1 ARG2
+    except IndexError as e:
+        print(
+                "No filename provided! Please enter a file name, e.g.\n",
+                "$ python plot.py FILENAME"
+                )
+        print(e)
+        return
 
-data = np.loadtxt(filename)   # Attempts to load filename into local variable data.
-print("Loading data from {}".format(filename))
+    print("Loading data from {}".format(filename))
+    try:
+        data = np.loadtxt(filename)   # Attempts to load filename into local variable data.
+    except ValueError as e:
+        print("I can't read from this file")
+        print(e)
+        return
 
-## Part 0
-# Figure out what arguments to add to the loadtxt function call
-# so that numbers are loaded into the local function 'data'.
-# Hint: look for arguments like 'skiprows' and 'delimiter'
-# Check by running:
-#   $ python plot.py raw-data/Sp15_245L_sect-001_group-1_glass.raw
-# at the command line.
-
-
-## Part 1
-# Figure out what columns and rows of data we need to plot
-# Stress (y-axis) vs Strain (x-axis)
-# plot raw-data/Sp15_245L_sect-001_group-1_glass.raw
-# Make sure to include axis labels and units!
-# plt.plot(xdata,ydata, arguments-to-make-plot-pretty)
-
-
-## Part 2
-# Check to see if your code in part 1 will plot all of the files in raw-data/
-# Edit the files (use git liberally here!) to make them more usable
+    ## Part 0
+    # Figure out what arguments to add to the loadtxt function call
+    # so that numbers are loaded into the local function 'data'.
+    # Hint: look for arguments like 'skiprows' and 'delimiter'
+    # Check by running:
+    #   $ python plot.py raw-data/Sp15_245L_sect-001_group-1_glass.raw
+    # at the command line.
 
 
-## Part 3
-# Use linear regression to calculate the slope of the linear part of
-# the stress-strain data. Plot your line against the data to make 
-# sure it makes sense! Use the slope of this line to calculate and print
-# the Young's modulus (with units!)
+    ## Part 1
+    # Figure out what columns and rows of data we need to plot
+    # Stress (y-axis) vs Strain (x-axis)
+    # plot raw-data/Sp15_245L_sect-001_group-1_glass.raw
+    # Make sure to include axis labels and units!
+    # plt.plot(xdata,ydata, arguments-to-make-plot-pretty)
 
 
-## Part 4
-# Modify your code to save your plots to a file and see if you can generate
-# plots and Young's moduli for all of the cleaned up files in your data 
-# directory. If you haven't already, this is a good time to add text to 
-# your .gitignore file so you're not committing the figures to your repository.
+    ## Part 2
+    # Check to see if your code in part 1 will plot all of the files in raw-data/
+    # Edit the files (use git liberally here!) to make them more usable
+
+
+    ## Part 3
+    # Use linear regression to calculate the slope of the linear part of
+    # the stress-strain data. Plot your line against the data to make
+    # sure it makes sense! Use the slope of this line to calculate and print
+    # the Young's modulus (with units!)
+
+
+    ## Part 4
+    # Modify your code to save your plots to a file and see if you can generate
+    # plots and Young's moduli for all of the cleaned up files in your data
+    # directory. If you haven't already, this is a good time to add text to
+    # your .gitignore file so you're not committing the figures to your repository.
 
 
 
+if __name__ == "__main__":
+    main()
